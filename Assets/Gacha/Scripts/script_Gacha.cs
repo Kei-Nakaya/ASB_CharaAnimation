0c7c610be33862718ed18bf07b2fa89d1b3ba78c
KeiNakaya <kei.n.mp4@gmail.com>
2024-11-22 12:53:23 +0900

    前期後期データを集約

---
+++ Assets/Gacha/Scripts/script_Gacha.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class script_Gacha : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        //【memo】一時停止(時間に×0をする)
+        Time.timeScale = 0;
+    }
+
+    public void Restart()
+    {
+        //【memo】再生(時間に×1をする)
+        Time.timeScale = 1;
+    }
+
+    public void ChangeScene()
+    {
+        //【memo】シーン番号 1 以上 3 未満 をランダムでロード
+        int index = Random.Range(1, 3);
+        SceneManager.LoadScene(index);
+        Debug.Log("Scene Loaded");
+    }
+
+    public void Return()
+    {
+        //【memo】シーン番号 0 をロード
+        SceneManager.LoadScene(0);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
